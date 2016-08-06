bfb6146c2812ac1ce10f05271958e7e62e7fe42b
nitely <ecastroborsani@gmail.com>
2016-08-06 18:59:43 -0300

    tests suffixes

--- runtests.py
+++ runtests.py
@@ -4,14 +4,26 @@
 from __future__ import unicode_literals
 import unittest
 
+import six
+
 from v8cffi.platform import platform
+
+if six.PY2:
+    PATTERN = 'test*_py2*.py'
+else:
+    PATTERN = 'test*_py3*.py'
 
 
 def start():
+    unittest.defaultTestLoader.discover('tests')
+
     with platform as _:
         unittest.main(
             module=None,
-            argv=['v8cffi', 'discover'])
+            argv=[
+                'v8cffi', 'discover',
+                '-s', 'tests',
+                '-p', PATTERN])
 
 
 if __name__ == '__main__':
--- tests/tests_async_context_py3.py
+++ tests/tests_async_context_py3.py
@@ -16,9 +16,6 @@ from v8cffi.async.vm import VM
 
 logging.disable(logging.CRITICAL)
 
-if six.PY2:
-    raise unittest.SkipTest()
-
 
 def async_test(func):
     def wrapper(test_klass, *args, **kwargs):
